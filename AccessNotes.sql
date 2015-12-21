5a2584b0461c3e5e85f300a4f5169a94d961dcbf
Maureen Callahan <maureen.callahan@gmail.com>
2015-12-21 09:11:47 -0500

    Update AccessNotes.sql

--- AccessNotes.sql
+++ AccessNotes.sql
@@ -1,12 +1,37 @@
-USE schema;
 SELECT 
-    note.resource_id, cast(note.notes AS CHAR(10000) CHARACTER SET utf8)
+    note.resource_id,
+    NULL,
+    CAST(note.notes AS CHAR (10000) CHARACTER SET UTF8),
+    rights_restriction.begin,
+    rights_restriction.end,
+    rights_restriction_type.restriction_type_id
 FROM
     note
         JOIN
     resource ON resource.id = note.resource_id
+        LEFT JOIN
+    rights_restriction ON resource.id = rights_restriction.resource_id
+        LEFT JOIN
+    rights_restriction_type ON rights_restriction.id = rights_restriction_type.rights_restriction_id
 WHERE
-    (resource_id IS NOT NULL
-        OR note.archival_object_id IS NOT NULL)
-        AND note.notes LIKE '%accessrestrict%'
-        AND resource.repo_id = 12;
+    note.notes LIKE '%accessrestrict%'
+        AND resource.repo_id = 12 
+UNION ALL SELECT 
+    archival_object.root_record_id,
+    note.archival_object_id,
+    CAST(note.notes AS CHAR (10000) CHARACTER SET UTF8),
+    rights_restriction.begin,
+    rights_restriction.end,
+    rights_restriction_type.restriction_type_id
+FROM
+    note
+        JOIN
+    archival_object ON archival_object.id = note.archival_object_id
+        LEFT JOIN
+    rights_restriction ON archival_object.id = rights_restriction.archival_object_id
+        LEFT JOIN
+    rights_restriction_type ON rights_restriction.id = rights_restriction_type.rights_restriction_id
+WHERE
+    note.notes LIKE '%accessrestrict%'
+        AND archival_object.repo_id = 12
+ORDER BY resource_id ASC;
