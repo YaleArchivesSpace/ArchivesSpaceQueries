2ab3b21a6713bc7e1bfbdb60cc756e8a121b57f1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-21 15:58:48 +0800

    Delete Data Monitoring directory

--- Data Monitoring/SeriesStringInUnitid.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    *
-FROM
-    archival_object
-WHERE
-    (level_id = 897 OR level_id = 900)
-        AND (component_id LIKE '%Series%'
-        OR component_id LIKE '%series%')
-ORDER BY id DESC;
