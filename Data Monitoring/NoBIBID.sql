303a8c33ee898495caa799fed67aceb84e4040cc
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-21 16:03:28 +0800

    Delete Data Monitoring directory

--- Data Monitoring/NoBIBID.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    *
-FROM
-    resource
-    join user_defined on resource.id = user_defined.resource_id
-WHERE
-    resource_id IS NOT NULL
-        AND string_2 IS NULL;
--- Data Monitoring/UnitidNotArabic.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    *
-FROM
-    archival_object
-WHERE
-    level_id = 897
-        AND NOT (component_id LIKE '%accession%'
-        OR component_id LIKE '%Accession%')
-        AND NOT component_id REGEXP '^[0-9.]+$'
